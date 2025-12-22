09e249d56f61347d79e098cc890d5a6411e5fbef
Vipul <vvm3875@gmail.com>
2025-12-22 18:40:52 +0530

    Dockerfile changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,12 +71,18 @@ CHANNEL_LAYERS = {
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+        "OPTIONS": {
+            "sslmode": os.environ.get("POSTGRES_SSLMODE", "require"),
+        },
     }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
